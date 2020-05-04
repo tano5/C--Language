94494a9ab6644deefa9c652cc03698e151d18a4e
tan.o5 <tanushdesai.05@gmail.com>
2020-05-04 09:04:22 +0530

    Update Structure.c

--- Structure of C Program/Structure.c
+++ Structure of C Program/Structure.c
@@ -1,14 +1,14 @@
 #include<stdio.h> 										/*LIBRARY FILE ACCESS*/
 
-/* program to calculate the area of a cirlce*/     		/* TITLE(COMMENT) */
+/* program to calculate the area of a cirlce*/     						/* TITLE(COMMENT) */
 
-int main()												/* FUNCTION HEADING */
+int main()											/* FUNCTION HEADING */
 {
 	float radius, area;									/* VARIABLE REQUEST */
 	
-	printf("Radius = ");								/* OUTPUT STATEMENT[PROMPT] */
-	scanf("%f", &radius);								/* INPUT STATEMENT */
-	area = 3.14 * radius * radius;						/* ASSIGNMENT STATEMENT */
-	printf("Area= %f", area);							/* OUTPUT STATEMENT */
+	printf("Radius = ");									/* OUTPUT STATEMENT[PROMPT] */
+	scanf("%f", &radius);									/* INPUT STATEMENT */
+	area = 3.14 * radius * radius;								/* ASSIGNMENT STATEMENT */
+	printf("Area= %f", area);								/* OUTPUT STATEMENT */
 	return 0;
 }
